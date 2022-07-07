16484be5fd9b987fbd4684283b67c1439faceacc
Adrian Khaskin <haskin.dev@gmail.com>
2022-07-07 12:01:13 -0400

    Changed naming scheme

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 
-public class FizzBuzzShould {
+public class FizzBuzzTest {
 
     private FizzBuzz fizzBuzz = new FizzBuzz();
     private static final String FIZZ = "Fizz";
@@ -12,25 +12,25 @@ public class FizzBuzzShould {
 
     // Test the "starts with f" case first - this should pass.
     @Test
-    void return_Fizz_when_string_starts_with_f() {
+    void Fizz_when_input_starts_with_f() {
         assertEquals(FIZZ, fizzBuzz.fizzBuzzString("fizz"));
     }
 
     // Test the "ends with b" case - this should also pass.
     @Test
-    void return_Buzz_when_string_starts_with_b() {
+    void Buzz_when_input_starts_with_b() {
         assertEquals(BUZZ, fizzBuzz.fizzBuzzString("b"));
     }
 
     // Test the "starts with f and ends with b" case - this will not pass.
     @Test
-    void return_FizzBuzz_when_string_starts_with_f_and_ends_with_b() {
+    void FizzBuzz_when_input_starts_with_f_and_ends_with_b() {
         assertEquals(FIZZ_BUZZ, fizzBuzz.fizzBuzzString("fb"));
     }
 
     // Add a test for when str is null - this will not pass.
     @Test
-    void return_null_when_string_is_null() {
+    void null_when_input_is_null() {
         assertNull(fizzBuzz.fizzBuzzString(null));
     }
 }
